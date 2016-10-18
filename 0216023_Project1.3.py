d6a011cea1fb1d32cf59394ea77625a7554b4a66
Taku <gcobs135102@gmail.com>
2016-10-18 21:26:29 +0800

    received AES & Initial Vector

--- 0216023_Project1.3.py
+++ 0216023_Project1.3.py
@@ -28,3 +28,55 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 	msg_size = struct.unpack('i', sock.recv(4))
 	received = str(sock.recv(int(msg_size[0])), "utf-8")
 	print('TA send : ', received)
+
+	# Send public pem file to server
+	with open('public.pem', 'rb') as f:
+		myPubKey = serialization.load_pem_public_key(
+			f.read(),
+			backend=default_backend()
+		)
+		f.close()
+	myPubPem = myPubKey.public_bytes(
+		encoding=serialization.Encoding.PEM,
+		format=serialization.PublicFormat.SubjectPublicKeyInfo
+	)
+	msg_size = len(str(myPubPem, 'utf-8'))
+	byte_msg_size = struct.pack('i', msg_size)
+	sock.sendall(byte_msg_size)
+	sock.sendall(myPubPem)
+	print('I send my RSA public key :\n', str(myPubPem, 'utf-8'))
+
+	# Receive AES Session Key from server
+	msg_size = struct.unpack('i', sock.recv(4))
+	encryptedAESKey = sock.recv(int(msg_size[0]))
+	print('Received C1 :\n', encryptedAESKey)
+	with open('private.pem', 'rb') as f:
+		myPriKey = serialization.load_pem_private_key(
+			f.read(),
+			password=None, 
+			backend=default_backend()
+		)
+		f.close()
+	AESKey = myPriKey.decrypt(
+	    encryptedAESKey,
+	    padding.OAEP(
+	        mgf=padding.MGF1(algorithm=hashes.SHA1()),
+	        algorithm=hashes.SHA1(),
+	        label=None
+	    )
+	)
+	print('ASS Session Key :\n', AESKey)
+
+	# Receive Initial Vector from Server
+	msg_size = struct.unpack('i', sock.recv(4))
+	encryptedIV = sock.recv(int(msg_size[0]))
+	print('Received C2 :\n', encryptedIV)
+	IV = myPriKey.decrypt(
+	    encryptedIV,
+	    padding.OAEP(
+	        mgf=padding.MGF1(algorithm=hashes.SHA1()),
+	        algorithm=hashes.SHA1(),
+	        label=None
+	    )
+	)
+	print('Initial Vector :\n', IV)
